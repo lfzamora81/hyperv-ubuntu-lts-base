a8d22cdde2a76e197dfb4f9f9686dbf4f81791b0
Lennon Zamora <me@lennonzamora.com>
2021-06-03 20:24:51 -0500

    added convert to dynamic

--- convert.ps1
+++ convert.ps1
@@ -1,1 +1,2 @@
-Convert-VHD -Path .\livecd.ubuntu-cpc.azure.vhd -DestinationPath .\livecd.ubuntu-cpc.azure.vhdx
+Convert-VHD -VHDType Dynamic -Path .\livecd.ubuntu-cpc.azure.vhd -DestinationPath .\livecd.ubuntu-cpc.azure.vhdx
+Optimize-VHD livecd.ubuntu-cpc.azure.vhdx -Mode Full
